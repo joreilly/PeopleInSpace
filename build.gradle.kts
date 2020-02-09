8185e9b06cf65a2205cc7122c6f740457381be19
John O'Reilly <oreillyj@gmail.com>
2020-02-09 11:42:41 +0000

    start using kotlin gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+        jcenter()
+
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:${Versions.androidBuildToolsVersion}")
+        classpath(kotlin("gradle-plugin", version = "${Versions.kotlin}"))
+        classpath(kotlin("serialization", version = "${Versions.kotlin}"))
+        classpath("com.squareup.sqldelight:gradle-plugin:${Versions.sqlDelight}")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+tasks.register("clean").configure {
+    delete("build")
+}
+
