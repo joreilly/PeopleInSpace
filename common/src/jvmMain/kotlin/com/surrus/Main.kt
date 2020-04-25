e6e4144177f31b78ff7cfd7e1484b6faab7f7e22
John O'Reilly <oreillyj@gmail.com>
2020-04-25 14:37:00 +0100

    fix jdbc sqldelight driver setup

--- common/src/jvmMain/kotlin/com/surrus/Main.kt
+++ common/src/jvmMain/kotlin/com/surrus/Main.kt
@@ -1,10 +1,18 @@
 package com.surrus
 
 import com.surrus.common.remote.PeopleInSpaceApi
+import com.surrus.common.repository.PeopleInSpaceRepository
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.runBlocking
 
+@InternalCoroutinesApi
 fun main() = runBlocking {
     val api = PeopleInSpaceApi()
-    val people = api.fetchPeople()
-    println(people)
+    println(api.fetchPeople())
+
+    val repository = PeopleInSpaceRepository()
+    repository.fetchPeopleAsFlow().collect {
+        println(it)
+    }
 }
--- common/src/jvmMain/kotlin/com/surrus/common/repository/actual.kt
+++ common/src/jvmMain/kotlin/com/surrus/common/repository/actual.kt
@@ -6,6 +6,7 @@ import com.surrus.peopleinspace.db.PeopleInSpaceDatabase
 
 actual fun createDb(): PeopleInSpaceDatabase {
     val driver =  JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
+        .also { PeopleInSpaceDatabase.Schema.create(it) }
     return PeopleInSpaceDatabase(driver)
 }
 
