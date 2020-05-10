2c7c5418eed69fb10a001bc12716dc2128540bef
John O'Reilly <oreillyj@gmail.com>
2020-05-10 19:49:34 +0100

    build updates

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -28,6 +28,11 @@ dependencies {
 kotlin {
     target {
         useCommonJs()
-        browser()
+        browser {
+            // https://kotlinlang.org/docs/reference/javascript-dce.html#known-issue-dce-and-ktor
+            dceTask {
+                keep("ktor-ktor-io.\$\$importsForInline\$\$.ktor-ktor-io.io.ktor.utils.io")
+            }
+        }
     }
 }
