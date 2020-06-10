aaa439a0f212f8831e818b591adbf291d2c678f4
John O'Reilly <oreillyj@gmail.com>
2020-06-10 19:57:44 +0100

    update to compose dev13 + gradle plugin 4.2.0-alph01

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,8 +1,8 @@
 
 object Versions {
-    const val androidBuildToolsVersion = "4.1.0-alpha10"
+    const val androidBuildToolsVersion = "4.2.0-alpha01"
 
-    const val kotlin = "1.3.71"
+    const val kotlin = "1.3.72"
     const val kotlinCoroutines = "1.3.5-native-mt"
     const val koin = "3.0.0-alpha-2"
     const val ktor = "1.3.2"
@@ -16,7 +16,7 @@ object Versions {
     const val room = "2.1.0-beta01"
     const val lifecycle = "2.2.0-alpha01"
     const val fragment = "1.1.0-alpha09"
-    const val compose = "0.1.0-dev11"
+    const val compose = "0.1.0-dev13"
 
     const val junit = "4.12"
     const val coreTesting = "2.0.0"
