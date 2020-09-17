16042000735642595bed2f733696d7e98a97f7ff
John O'Reilly <oreillyj@gmail.com>
2020-09-17 18:29:43 +0100

    web gradle cleanup

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -21,11 +21,6 @@ dependencies {
 kotlin {
     target {
         useCommonJs()
-        browser {
-            // https://kotlinlang.org/docs/reference/javascript-dce.html#known-issue-dce-and-ktor
-            dceTask {
-                keep("ktor-ktor-io.\$\$importsForInline\$\$.ktor-ktor-io.io.ktor.utils.io")
-            }
-        }
+        browser()
     }
 }
