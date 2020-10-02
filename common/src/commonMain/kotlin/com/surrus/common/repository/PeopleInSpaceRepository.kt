186dcfafec92d1854ee7453200a0ede525ef128c
John O'Reilly <oreillyj@gmail.com>
2020-10-02 20:18:09 +0100

    update image url

--- common/src/commonMain/kotlin/com/surrus/common/repository/PeopleInSpaceRepository.kt
+++ common/src/commonMain/kotlin/com/surrus/common/repository/PeopleInSpaceRepository.kt
@@ -17,7 +17,7 @@ expect fun createDb() : PeopleInSpaceDatabase?
 val personImages = mapOf(
     "Chris Cassidy" to "https://www.nasa.gov/sites/default/files/styles/side_image/public/thumbnails/image/9368855148_f79942efb7_o.jpg?itok=-w5yoryN",
     "Anatoly Ivanishin" to "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Anatoli_Ivanishin_2011.jpg/440px-Anatoli_Ivanishin_2011.jpg",
-    "Ivan Vagner" to "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Ivan_Vagner_%28Jsc2020e014992%29.jpg/440px-Ivan_Vagner_%28Jsc2020e014992%29.jpg",
+    "Ivan Vagner" to "http://www.spacefacts.de/more/cosmonauts/photo/vagner_ivan_3.jpg",
 )
 
 val personBios = mapOf(
