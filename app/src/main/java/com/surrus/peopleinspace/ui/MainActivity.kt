b1512a5006c50c8d135a8d1c91388e77bdb025e5
John O'Reilly <oreillyj@gmail.com>
2020-11-16 08:34:46 +0000

    update images/bios for new astronauts

--- app/src/main/java/com/surrus/peopleinspace/ui/MainActivity.kt
+++ app/src/main/java/com/surrus/peopleinspace/ui/MainActivity.kt
@@ -2,16 +2,11 @@ package com.surrus.peopleinspace.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Icon
 import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumnFor
-import androidx.compose.material.Text
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
