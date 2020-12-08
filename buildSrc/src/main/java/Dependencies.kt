718ad8dcc504b73ab17874ad4f57b3de157a2d23
John O'Reilly <oreillyj@gmail.com>
2020-12-08 20:04:35 +0000

    kotlin 1.4.21

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,6 +1,6 @@
 
 object Versions {
-    const val kotlin = "1.4.20"
+    const val kotlin = "1.4.21"
     const val kotlinCoroutines = "1.4.2-native-mt"
     const val ktor = "1.4.0"
     const val kotlinxSerialization = "1.0.0-RC"
