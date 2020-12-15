b8fdfe3b9f5b68acf7beeaa1a20f000ed6686ebd
Marc Reichelt <marc.reichelt@iteratec.com>
2020-12-15 16:42:48 +0100

    Actually load people from space

--- command-line/src/nativeMain/kotlin/PeopleInSpace.kt
+++ command-line/src/nativeMain/kotlin/PeopleInSpace.kt
@@ -1,3 +1,22 @@
+import com.surrus.common.di.initKoin
+import com.surrus.common.remote.PeopleInSpaceApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+
 fun main() {
+    runBlocking(context = Dispatchers.Unconfined) {
+        Platform.isMemoryLeakCheckerActive = false
+        val koin = initKoin().koin
+        val peopleInSpaceApi = koin.get<PeopleInSpaceApi>()
+        println(loadAstronauts(peopleInSpaceApi).joinToString("\n"))
+    }
+}
+
+private suspend fun loadAstronauts(peopleInSpaceApi: PeopleInSpaceApi): List<String> {
     println("Loading astronauts from space...")
+    return peopleInSpaceApi
+        .fetchPeople()
+        .people
+        .map { it.name }
+        .sorted()
 }
