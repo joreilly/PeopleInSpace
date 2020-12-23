bb4d8779992ecec2fae4ab81fc8bdb84566479ea
John O'Reilly <oreillyj@gmail.com>
2020-12-23 12:19:05 +0000

    update to 7.0.0-alpha03 plugin + workaround for issue it causes: fixes #23

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
     }
     dependencies {
         classpath("xml-apis:xml-apis:1.4.01")
-        classpath("com.android.tools.build:gradle:7.0.0-alpha02")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+        classpath("com.android.tools.build:gradle:7.0.0-alpha03")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.20")
         classpath("org.jetbrains.kotlin:kotlin-serialization:${Versions.kotlin}")
         classpath("com.squareup.sqldelight:gradle-plugin:${Versions.sqlDelight}")
     }
