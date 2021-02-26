9b9137dfc55da81e714e6b1719a56e6c582d30e0
John O'Reilly <oreillyj@gmail.com>
2021-02-26 08:35:06 +0000

    gradle plugin  7.0.0-alpha08

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-alpha07")
+        classpath("com.android.tools.build:gradle:7.0.0-alpha08")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
         classpath("org.jetbrains.kotlin:kotlin-serialization:${Versions.kotlin}")
         classpath("com.squareup.sqldelight:gradle-plugin:${Versions.sqlDelight}")
