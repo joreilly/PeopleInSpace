a50419b66ecbf1c0424bb60b1ceb9e9d262d67b0
John O'Reilly <oreillyj@gmail.com>
2021-04-05 18:32:52 +0100

    ktor 1.5.3, kotlinx serialization 1.1.0

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,8 +2,8 @@
 object Versions {
     const val kotlin = "1.4.31"
     const val kotlinCoroutines = "1.4.3-native-mt"
-    const val ktor = "1.5.2"
-    const val kotlinxSerialization = "1.0.1"
+    const val ktor = "1.5.3"
+    const val kotlinxSerialization = "1.1.0"
     const val koin = "3.0.1-beta-1"
     const val sqlDelight = "1.4.3"
     const val kermit = "0.1.8"
