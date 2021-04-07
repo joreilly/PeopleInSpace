05d08fe7a900d2149ec40f5d6294014c648914e9
John O'Reilly <oreillyj@gmail.com>
2021-04-07 18:33:12 +0100

    coil 0.7.1

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -12,7 +12,7 @@ object Versions {
     const val slf4j = "1.7.30"
     const val compose = "1.0.0-beta04"
     const val nav_compose = "1.0.0-alpha09"
-    const val accompanist = "0.7.0"
+    const val accompanist = "0.7.1"
 
     const val junit = "4.13"
     const val testRunner = "1.3.0"
