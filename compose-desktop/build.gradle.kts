cd0b017832b97e5749b42d60bfa9cc1acecdbd7f
Neo <c.min.tseng@gmail.com>
2021-04-29 18:04:07 +0800

    modify gradle let  Compose for desktop run @ compose desktop gradle task group

--- compose-desktop/build.gradle.kts
+++ compose-desktop/build.gradle.kts
@@ -1,10 +1,11 @@
 import org.jetbrains.compose.compose
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
-    kotlin("jvm")
+    //Ref :  https://github.com/JetBrains/compose-jb/blob/master/examples/todoapp/desktop/build.gradle.kts
+    kotlin("multiplatform")  // kotlin("jvm") doesn't work well in IDEA/AndroidStudio (https://github.com/JetBrains/compose-jb/issues/22)
     id("org.jetbrains.compose") version "0.4.0-build184"
-    application
 }
 
 group = "me.joreilly"
@@ -16,15 +17,40 @@ repositories {
     maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
-dependencies {
-    implementation(compose.desktop.currentOs)
-    implementation(project(":common"))
+kotlin {
+    jvm {
+        withJava()
+    }
+
+    sourceSets {
+        named("jvmMain") {
+            kotlin.srcDir("src/main")
+            dependencies {
+                implementation(compose.desktop.currentOs)
+                implementation(project(":common"))
+            }
+        }
+    }
 }
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
 
-application {
-    mainClass.set("MainKt")
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "PeopleInSpace"
+            packageVersion = "1.0.0"
+
+            windows {
+                menuGroup = "PeopleInSpace"
+                // see https://wixtoolset.org/documentation/manual/v3/howtos/general/generate_guids.html
+                upgradeUuid = "BF9CDA6A-1391-46D5-9ED5-383D6E68CCEB"
+            }
+        }
+    }
 }
