3a0ec08955ef52cb11d9f1415716c51d9a3663b2
Shreyas <shreyaspatilg@gmail.com>
2021-06-06 23:42:50 +0530

    Add List components

---
+++ web/src/main/kotlin/components/materialui/List.kt
@@ -0,0 +1,18 @@
+@file:JsModule("@material-ui/core")
+@file:JsNonModule
+
+package components.materialui
+
+import react.*
+
+@JsName("List")
+external val List: RClass<dynamic>
+
+@JsName("ListItem")
+external val ListItem: RClass<dynamic>
+
+@JsName("ListItemAvatar")
+external val ListItemAvatar: RClass<dynamic>
+
+@JsName("ListItemText")
+external val ListItemText: RClass<dynamic>
