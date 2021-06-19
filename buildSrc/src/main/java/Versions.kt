7bf1fb646019f06b28aae4284855b583b6269ad0
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-06-19 22:07:18 +0430

    Separate buildSrc configs

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,26 @@
+object Versions {
+    const val kotlin = "1.5.10"
+    const val kotlinCoroutines = "1.5.0-native-mt"
+    const val ktor = "1.6.0"
+    const val kotlinxSerialization = "1.2.1"
+    const val koin = "3.1.0"
+    const val sqlDelight = "1.5.0"
+    const val kermit = "0.1.9"
+
+    const val slf4j = "1.7.30"
+    const val compose = "1.0.0-beta09"
+    const val nav_compose = "2.4.0-alpha03"
+    const val accompanist = "0.12.0"
+
+    const val junit = "4.13"
+    const val mockito = "3.7.7"
+    const val testRunner = "1.3.0"
+
+    const val gradle = "7.1.0-alpha02"
+    const val kotlinterGradle = "3.4.5"
+    const val shadow = "7.0.0"
+
+    const val androidMinSdk = 21
+    const val androidCompileSdk = 29
+    const val androidTargetSdk = androidCompileSdk
+}
