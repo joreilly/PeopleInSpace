8aa3865e785c752b136dcf2da767fff4cb7f3b4d
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-06-19 16:15:16 +0430

    Replace ktlint-gradle with kotlinter-gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,25 @@
-
 buildscript {
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
+        // For kotlinter-gradle
+        maven(uri("https://plugins.gradle.org/m2/"))
     }
+
     dependencies {
         classpath("com.android.tools.build:gradle:7.1.0-alpha02")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
         classpath("org.jetbrains.kotlin:kotlin-serialization:${Versions.kotlin}")
         classpath("com.squareup.sqldelight:gradle-plugin:${Versions.sqlDelight}")
         classpath("gradle.plugin.com.github.jengelman.gradle.plugins:shadow:7.0.0")
-    }
-    plugins {
-        id("org.jlleitschuh.gradle.ktlint") version Versions.ktlintGradle
+        // kotlinter-gradle
+        classpath("org.jmailen.gradle:kotlinter-gradle:3.4.5")
     }
 }
 
 allprojects {
-    apply(plugin = "org.jlleitschuh.gradle.ktlint") // Version should be inherited from parent
+    apply(plugin = "org.jmailen.kotlinter")
 
     repositories {
         google()
