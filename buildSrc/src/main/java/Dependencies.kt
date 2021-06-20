01b402d81dc21db3b5e4cccf1c5c2cea5a191b9d
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-06-20 17:30:56 +0430

    Better organize build dependencies

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,47 +1,46 @@
 object Versions {
-    const val kotlin = "1.5.10"
-    const val kotlinCoroutines = "1.5.0-native-mt"
-    const val ktor = "1.6.0"
-    const val kotlinxSerialization = "1.2.1"
-    const val koin = "3.1.0"
-    const val sqlDelight = "1.5.0"
-    const val kermit = "0.1.9"
-    const val kotlinxHtmlJs = "0.7.3"
-    const val kotlinStyled = "5.2.1-pre.146-kotlin-1.4.30"
-    const val kotlinReact = "17.0.1-pre.146-kotlin-1.4.30"
-    const val kotlinReactDom = "17.0.1-pre.146-kotlin-1.4.30"
-    const val kotlinReactRouterDom = "5.1.2-pre.110-kotlin-1.4.0"
-
-    const val slf4j = "1.7.30"
-    const val compose = "1.0.0-beta09"
-    const val nav_compose = "2.4.0-alpha03"
-    const val accompanist = "0.12.0"
-
-    const val logback = "1.2.3"
-
-    const val junit = "4.13.2"
-    const val mockito = "3.7.7"
-    const val testRunner = "1.3.0"
-    const val testCore = "1.3.0"
-    const val robolectric = "4.4"
-
-    const val gradle = "7.1.0-alpha02"
-    const val kotlinterGradle = "3.4.5"
-    const val shadow = "7.0.0"
-
-    const val material = "1.3.0"
-
-    const val lifecycleKtx = "2.4.0-alpha01"
-    const val lifecycleRuntimeKtx = lifecycleKtx
-    const val lifecycleViewmodelKtx = lifecycleKtx
-
-    const val activityCompose = "1.3.0-beta2"
-
-    const val osmdroidAndroid = "6.1.10"
-
     const val androidMinSdk = 21
     const val androidCompileSdk = 29
     const val androidTargetSdk = androidCompileSdk
+
+    const val kotlin = "1.5.10"
+    const val gradle = "7.1.0-alpha02"
+
+    const val kotlinCoroutines = "1.5.0-native-mt"
+    const val koin = "3.1.0"
+    const val ktor = "1.6.0"
+    const val kotlinxSerialization = "1.2.1"
+    const val kotlinxHtmlJs = "0.7.3"
+
+    const val compose = "1.0.0-beta09"
+    const val navCompose = "2.4.0-alpha03"
+    const val accompanist = "0.12.0"
+
+    const val junit = "4.13.2"
+    const val mockito = "3.7.7"
+    const val testCore = "1.3.0"
+    const val testRunner = "1.3.0"
+    const val robolectric = "4.4"
+
+    const val sqlDelight = "1.5.0"
+    const val shadow = "7.0.0"
+    const val kotlinterGradle = "3.4.5"
+
+    const val material = "1.3.0"
+    const val activityCompose = "1.3.0-beta2"
+    const val lifecycleKtx = "2.4.0-alpha01"
+    const val lifecycleRuntimeKtx = lifecycleKtx
+    const val lifecycleViewmodelKtx = lifecycleKtx
+    const val osmdroidAndroid = "6.1.10"
+
+    const val kotlinReact = "17.0.1-pre.146-kotlin-1.4.30"
+    const val kotlinReactDom = "17.0.1-pre.146-kotlin-1.4.30"
+    const val kotlinReactRouterDom = "5.1.2-pre.110-kotlin-1.4.0"
+    const val kotlinStyled = "5.2.1-pre.146-kotlin-1.4.30"
+
+    const val slf4j = "1.7.30"
+    const val logback = "1.2.3"
+    const val kermit = "0.1.9"
 }
 
 object Deps {
@@ -85,7 +84,7 @@ object Deps {
         const val uiTooling = "androidx.compose.ui:ui-tooling:${Versions.compose}"
         const val foundationLayout = "androidx.compose.foundation:foundation-layout:${Versions.compose}"
         const val material = "androidx.compose.material:material:${Versions.compose}"
-        const val navigation = "androidx.navigation:navigation-compose:${Versions.nav_compose}"
+        const val navigation = "androidx.navigation:navigation-compose:${Versions.navCompose}"
         const val accompanistCoil = "com.google.accompanist:accompanist-coil:${Versions.accompanist}"
         const val accompanistPlaceholder = "com.google.accompanist:accompanist-placeholder:${Versions.accompanist}"
     }
@@ -131,8 +130,8 @@ object Deps {
     }
 
     object Log {
-        const val logback = "ch.qos.logback:logback-classic:${Versions.logback}"
         const val slf4j = "org.slf4j:slf4j-simple:${Versions.slf4j}"
+        const val logback = "ch.qos.logback:logback-classic:${Versions.logback}"
         const val kermit = "co.touchlab:kermit:${Versions.kermit}"
     }
 }
