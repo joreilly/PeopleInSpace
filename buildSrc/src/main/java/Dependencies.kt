6f86cb90a72cf85acfd7138917760ad25cae2c89
John O'Reilly <oreillyj@gmail.com>
2021-07-28 18:18:24 +0100

    Jetpack Compose 1.0

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -11,9 +11,9 @@ object Versions {
     const val kotlinxSerialization = "1.2.1"
     const val kotlinxHtmlJs = "0.7.3"
 
-    const val compose = "1.0.0-rc02"
+    const val compose = "1.0.0"
     const val wearCompose = "1.0.0-alpha02"
-    const val navCompose = "2.4.0-alpha04"
+    const val navCompose = "2.4.0-alpha05"
     const val accompanist = "0.14.0"
 
     const val junit = "4.12"
@@ -27,7 +27,7 @@ object Versions {
     const val kotlinterGradle = "3.4.5"
 
     const val material = "1.4.0"
-    const val activityCompose = "1.3.0-rc02"
+    const val activityCompose = "1.3.0"
     const val lifecycleKtx = "2.4.0-alpha01"
     const val lifecycleRuntimeKtx = lifecycleKtx
     const val lifecycleViewmodelKtx = lifecycleKtx
