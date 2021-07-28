fad27a2adb431c1653115033df6c6774ffff5a5f
John O'Reilly <oreillyj@gmail.com>
2021-07-28 18:27:34 +0100

    gradle plugin 7.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
 
     dependencies {
         // keeping this here to allow AS to automatically update
-        classpath("com.android.tools.build:gradle:7.0.0-rc01")
+        classpath("com.android.tools.build:gradle:7.0.0")
 
         with(Deps.Gradle) {
             classpath(kotlin)
