72b93622c81ac3cecaa3adeff4df4e8e423eb24b
Yuri Schimke <yuri@schimke.ee>
2021-10-29 18:07:56 +0300

    ISS Map and deeplinks

--- wearApp/src/main/java/com/surrus/peopleinspace/MainActivity.kt
+++ wearApp/src/main/java/com/surrus/peopleinspace/MainActivity.kt
@@ -5,6 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.navigation.NavDeepLink
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 import androidx.wear.compose.material.ExperimentalWearMaterialApi
 import androidx.wear.compose.navigation.SwipeDismissableNavHost
 import androidx.wear.compose.navigation.composable
@@ -16,9 +20,11 @@ import org.koin.android.ext.android.inject
 sealed class Screen(val route: String) {
     object PersonList : Screen("personList")
     object PersonDetails : Screen("personDetails")
+    object IssMap : Screen("issMap")
 }
 
 const val PERSON_NAME_NAV_ARGUMENT = "personName"
+const val DEEPLINK_URI = "peopleinspace://peopleinspace.dev/"
 
 class MainActivity : ComponentActivity() {
     private val imageLoader: ImageLoader by inject()
@@ -43,19 +49,38 @@ class MainActivity : ComponentActivity() {
                     startDestination = Screen.PersonList.route
                 ) {
 
-                    composable(Screen.PersonList.route) {
-                        PersonListScreen(personSelected = {
-                            navController.navigate(Screen.PersonDetails.route + "/${it.name}")
-                        })
+                    composable(
+                        route = Screen.PersonList.route,
+                        deepLinks = listOf(NavDeepLink(DEEPLINK_URI + "personList"))
+                    ) {
+                        PersonListScreen(
+                            personSelected = {
+                                navController.navigate(Screen.PersonDetails.route + "/${it.name}")
+                            },
+                            issMapClick = {
+                                navController.navigate(Screen.IssMap.route)
+                            })
                     }
 
                     composable(
-                        route = Screen.PersonDetails.route + "/{$PERSON_NAME_NAV_ARGUMENT}"
+                        route = Screen.PersonDetails.route + "/{$PERSON_NAME_NAV_ARGUMENT}",
+                        arguments = listOf(
+                            navArgument(PERSON_NAME_NAV_ARGUMENT, builder = {
+                                this.type = NavType.StringType
+                            })),
+                        deepLinks = listOf(navDeepLink { uriPattern = DEEPLINK_URI + "personList/{${PERSON_NAME_NAV_ARGUMENT}}" })
                     ) { navBackStackEntry ->
                         val personName = navBackStackEntry.arguments?.getString(PERSON_NAME_NAV_ARGUMENT)
                         personName?.let {
                             PersonDetailsScreen(personName)
                         }
+                    }
+
+                    composable(
+                        route = Screen.IssMap.route,
+                        deepLinks = listOf(NavDeepLink(DEEPLINK_URI + "issMap"))
+                    ) {
+                        IssMap()
                     }
                 }
             }
--- wearApp/src/main/java/com/surrus/peopleinspace/PersonListScreen.kt
+++ wearApp/src/main/java/com/surrus/peopleinspace/PersonListScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -13,20 +14,25 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.ButtonDefaults
 import androidx.wear.compose.material.Card
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.ScalingLazyColumn
@@ -45,12 +51,13 @@ const val NoPeopleTag = "NoPeople"
 @Composable
 fun PersonListScreen(
     personSelected: (person: Assignment) -> Unit,
+    issMapClick: () -> Unit,
     peopleInSpaceViewModel: PeopleInSpaceViewModel = getViewModel()
 ) {
     val peopleState by peopleInSpaceViewModel.peopleInSpace.collectAsState()
     val scrollState = rememberScalingLazyListState()
     RotaryEventState(scrollState)
-    PersonListScreen(peopleState, personSelected, scrollState)
+    PersonListScreen(peopleState, personSelected, issMapClick, scrollState)
 }
 
 @OptIn(ExperimentalAnimationApi::class)
@@ -58,6 +65,7 @@ fun PersonListScreen(
 fun PersonListScreen(
     people: List<Assignment>?,
     personSelected: (person: Assignment) -> Unit,
+    issMapClick: () -> Unit,
     scrollState: ScalingLazyListState = rememberScalingLazyListState(),
 ) {
     MaterialTheme {
@@ -67,7 +75,7 @@ fun PersonListScreen(
         ) {
             if (people != null) {
                 if (people.isNotEmpty()) {
-                    PersonList(people, personSelected, scrollState)
+                    PersonList(people, personSelected, issMapClick, scrollState)
                 } else {
                     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                         Card(
@@ -87,19 +95,29 @@ fun PersonListScreen(
 fun PersonList(
     people: List<Assignment>,
     personSelected: (person: Assignment) -> Unit,
+    issMapClick: () -> Unit,
     scrollState: ScalingLazyListState = rememberScalingLazyListState(),
 ) {
-    val paddingHeight = if (LocalConfiguration.current.isScreenRound) 50.dp else 8.dp
     ScalingLazyColumn(
-        contentPadding = PaddingValues(
-            start = 8.dp,
-            end = 8.dp,
-            top = paddingHeight,
-            bottom = paddingHeight
-        ),
+        contentPadding = PaddingValues(8.dp),
         modifier = Modifier.testTag(PersonListTag),
         state = scrollState,
     ) {
+        item {
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                Button(
+                    modifier = Modifier.size(ButtonDefaults.SmallButtonSize).wrapContentSize(),
+                    onClick = issMapClick
+                ) {
+                    // https://www.svgrepo.com/svg/170716/international-space-station
+                    Image(
+                        modifier = Modifier.scale(0.5f),
+                        painter = painterResource(id = R.drawable.ic_iss),
+                        contentDescription = "ISS Map"
+                    )
+                }
+            }
+        }
         items(people.size) { offset ->
             PersonView(people[offset], personSelected)
         }
@@ -197,7 +215,7 @@ fun PersonListSquarePreview() {
             "Buzz Aldrin",
             "https://nypost.com/wp-content/uploads/sites/2/2018/06/buzz-aldrin.jpg?quality=80&strip=all"
         )
-    ), personSelected = {})
+    ), personSelected = {}, issMapClick = {})
 }
 
 @Preview(
@@ -210,5 +228,5 @@ fun PersonListSquarePreview() {
 )
 @Composable
 fun PersonListSquareEmptyPreview() {
-    PersonListScreen(people = listOf(), personSelected = {})
+    PersonListScreen(people = listOf(), personSelected = {}, issMapClick = {})
 }
