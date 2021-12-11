3bdaab7c1156c2c918ab8fb3b282970fb9c8594c
John O'Reilly <oreillyj@gmail.com>
2021-12-11 16:18:25 +0000

    android gradle plugin 7.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
 
     dependencies {
         // keeping this here to allow AS to automatically update
-        classpath("com.android.tools.build:gradle:7.0.3")
+        classpath("com.android.tools.build:gradle:7.0.4")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${kotlinVersion}")
         classpath("org.jetbrains.kotlin:kotlin-serialization:${kotlinVersion}")
 
