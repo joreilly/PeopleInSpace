15d605f7bdee93d4d8d0302826c86b33a0d0e462
John O'Reilly <oreillyj@gmail.com>
2022-09-29 20:05:33 +0100

    Kotlin 1.7.20

--- compose-ios/build.gradle.kts
+++ compose-ios/build.gradle.kts
@@ -1,8 +1,5 @@
-import org.jetbrains.compose.compose
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
 import org.jetbrains.compose.experimental.dsl.IOSDevices
 
 buildscript {
