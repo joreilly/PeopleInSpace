7b264b26eb2188911d3f2ed779630529b8870c5a
John O'Reilly <oreillyj@gmail.com>
2023-02-18 19:32:01 +0000

    koin updates

--- app/src/main/java/com/surrus/peopleinspace/di/AppModule.kt
+++ app/src/main/java/com/surrus/peopleinspace/di/AppModule.kt
@@ -3,11 +3,11 @@ package com.surrus.peopleinspace.di
 import com.surrus.peopleinspace.issposition.ISSPositionViewModel
 import com.surrus.peopleinspace.persondetails.PersonDetailsViewModel
 import com.surrus.peopleinspace.personlist.PersonListViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appModule = module {
-    viewModel { PersonListViewModel(peopleInSpaceRepository = get()) }
-    viewModel { PersonDetailsViewModel(get(), get()) }
-    viewModel { ISSPositionViewModel(peopleInSpaceRepository = get()) }
+    viewModelOf(::PersonListViewModel)
+    viewModelOf(::PersonDetailsViewModel)
+    viewModelOf(::ISSPositionViewModel)
 }
