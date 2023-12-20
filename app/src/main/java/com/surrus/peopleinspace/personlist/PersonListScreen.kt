9efc5693921eebf29e5afdcff2effad5895f4687
John O'Reilly <oreillyj@gmail.com>
2023-12-20 19:31:54 +0000

    version updates

--- app/src/main/java/com/surrus/peopleinspace/personlist/PersonListScreen.kt
+++ app/src/main/java/com/surrus/peopleinspace/personlist/PersonListScreen.kt
@@ -4,24 +4,30 @@
 
 package com.surrus.peopleinspace.personlist
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.LocalContentAlpha
-import androidx.compose.material.pullrefresh.*
-import androidx.compose.material3.*
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
@@ -35,14 +41,13 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
 import com.surrus.common.remote.Assignment
-import com.surrus.peopleinspace.R
 import com.surrus.peopleinspace.ui.PersonProvider
-import com.surrus.peopleinspace.ui.PurpleGray50
+import org.koin.androidx.compose.getViewModel
+import com.surrus.peopleinspace.R
 import com.surrus.peopleinspace.ui.component.PeopleInSpaceGradientBackground
 import com.surrus.peopleinspace.ui.component.PeopleInSpaceTopAppBar
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import org.koin.androidx.compose.getViewModel
 
 const val PersonListTag = "PersonList"
 
@@ -95,8 +100,7 @@ fun PersonListScreen(
 
             Box(Modifier.pullRefresh(state)) {
                 LazyColumn(
-                    modifier = Modifier
-                        .testTag(PersonListTag)
+                    modifier = Modifier.testTag(PersonListTag)
                         .padding(innerPadding)
                         .consumeWindowInsets(innerPadding)
                         .fillMaxSize()
@@ -116,43 +120,33 @@ fun PersonListScreen(
 
 @Composable
 fun PersonView(person: Assignment, personSelected: (person: String) -> Unit) {
-    Card(
+
+    Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp),
-        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)
+            .clickable(onClick = { personSelected(person.name) })
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable(onClick = { personSelected(person.name) })
-                .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
 
-            val personImageUrl = person.personImageUrl ?: ""
-            if (personImageUrl.isNotEmpty()) {
-                AsyncImage(
-                    model = person.personImageUrl,
-                    contentDescription = person.name,
-                    contentScale = ContentScale.Fit,
-                    modifier = Modifier
-                        .size(60.dp)
-                        .clip(CircleShape)
-                        .background(color = PurpleGray50)
-                )
-            } else {
-                Spacer(modifier = Modifier.size(60.dp))
-                CircularProgressIndicator()
-            }
+        val personImageUrl = person.personImageUrl ?: ""
+        if (personImageUrl.isNotEmpty()) {
+            AsyncImage(
+                model = person.personImageUrl,
+                contentDescription = person.name,
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.size(60.dp)
+            )
+        } else {
+            Spacer(modifier = Modifier.size(60.dp))
+        }
 
-            Spacer(modifier = Modifier.size(12.dp))
+        Spacer(modifier = Modifier.size(12.dp))
 
-            Column {
-                Text(text = person.name, style = TextStyle(fontSize = 20.sp))
-                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-                    Text(text = person.craft, style = TextStyle(fontSize = 14.sp))
-                }
+        Column {
+            Text(text = person.name, style = TextStyle(fontSize = 20.sp))
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(text = person.craft, style = TextStyle(fontSize = 14.sp))
             }
         }
     }
