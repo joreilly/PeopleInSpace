370dfffc11c61cf930704335ac2fdfed87103a51
John O'Reilly <oreillyj@gmail.com>
2024-07-06 12:03:12 +0100

    version updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.kotlinx.serialization) apply false
     alias(libs.plugins.kmpNativeCoroutines) apply false
     alias(libs.plugins.sqlDelight) apply false
     alias(libs.plugins.gradleVersionsPlugin) apply false
